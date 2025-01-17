f8ce47a9e45c92ec00513216b96ffaa8558d2b7b
SalomeGarzon <134555793+SalomeGarzon@users.noreply.github.com>
2025-01-16 19:37:44 -0500

    Delete TIC directory

--- TIC/js/Family/family.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('nextButton').addEventListener('click', () => {
-    window.location.href = '/TIC2025/TIC/html/Family/family1.html'; // Cambia a la URL de la siguiente página
-  });
-  
